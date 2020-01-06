7e3d25dda7e4bc947c7c51b2f6c714254aa71bc4
Ramiro Gómez <code@ramiro.org>
2020-01-07 00:50:42 +0100

    Add markdown support.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 from fscache import __version__
 
 
-test_requirements = Path('requirements_dev.txt').read_text().splitlines()
+test_requirements = Path('requirements_test.txt').read_text().splitlines()
 
 setup(
     author='Ramiro Gómez',
@@ -22,6 +22,7 @@ setup(
     description='A Python package for caching data in the file system.',
     license='MIT license',
     long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='cache, caching, file system',
     name='fscache',
